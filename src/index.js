63b8a8f205a5c3c51df0b8eb361117ae6ebd1736
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 13:31:41 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    let check = array ? array.length : 0;
+    if (check) {
+        return Math.min.apply(Math, array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let check = array ? array.length : 0;
+    if (check) {
+        return Math.max.apply(Math, array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let check = array ? array.length : 0;
+    let result;
+    if (check) {
+        result = array.reduce((sum, current) => sum + current, 0) / array.length * 100;
+        return Math.round(result) / 100;
+    }
+    return 0;
+};
